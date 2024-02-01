02de123a7c266b23dabb1a72d2e7bc4cfe03543f
Vladislav Boyko <vlboyko11@gmail.com>
2024-02-01 11:51:30 +0200

    First commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,14 +18,6 @@ export const getServerSideProps: GetServerSideProps<ConnectionStatus> = async ()
     const client = await clientPromise;
     const db = client.db('calendar')
     const movies = await db.collection('movies').find({}).toArray()
-    // `await clientPromise` will use the default database passed in the MONGODB_URI
-    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
-    //
-    // `const client = await clientPromise`
-    // `const db = client.db("myDatabase")`
-    //
-    // Then you can execute queries against your database like so:
-    // db.find({}) or any of the MongoDB Node Driver commands
     console.log(movies)
     return {
       props: { isConnected: true, movies: JSON.parse(JSON.stringify(movies)) },
